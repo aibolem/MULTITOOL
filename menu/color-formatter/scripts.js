2cba583e21c619d1a8a767c867e423180b807f0c
Manshu Sharma <manshu.sharma@digite.com>
2023-01-15 23:04:56 +0530

    add: rgb to hsv & few mdl colors

--- menu/color-formatter/scripts.js
+++ menu/color-formatter/scripts.js
@@ -14,6 +14,35 @@ function rgbToHex(r, g, b) {
     return `#${R}${G}${B}`;
 }
 
+function rgbTohsv (r,g,b) {
+    var computedH = 0;
+    var computedS = 0;
+    var computedV = 0;
+   
+    //remove spaces from input RGB values, convert to int
+    var r = parseInt( (''+r).replace(/\s/g,''),10 ); 
+    var g = parseInt( (''+g).replace(/\s/g,''),10 ); 
+    var b = parseInt( (''+b).replace(/\s/g,''),10 ); 
+   
+    r=r/255; g=g/255; b=b/255;
+    var minRGB = Math.min(r,Math.min(g,b));
+    var maxRGB = Math.max(r,Math.max(g,b));
+   
+    // Black-gray-white
+    if (minRGB==maxRGB) {
+     computedV = minRGB;
+     return `hsv(0,0,${computedV})`;
+    }
+   
+    // Colors other than black-gray-white:
+    var d = (r==minRGB) ? g-b : ((b==minRGB) ? r-g : b-r);
+    var h = (r==minRGB) ? 3 : ((b==minRGB) ? 1 : 5);
+    computedH = 60*(h - d/(maxRGB - minRGB));
+    computedS = (maxRGB - minRGB)/maxRGB;
+    computedV = maxRGB;
+    return `hsv(${computedH},${computedS},${computedV})`;
+}
+
 function fill(){
     var color_val = document.getElementById("color");
     var red_val = document.getElementById("red").value
@@ -21,11 +50,13 @@ function fill(){
     var blue_val = document.getElementById("blue").value
     color_val.style.background = `rgb(${red_val}, ${green_val}, ${blue_val})`;
     rgb.innerHTML = `rgb(${red_val}, ${green_val}, ${blue_val})`;
+    hsv.innerHTML = rgbTohsv(red_val, green_val, blue_val);
     hex.innerHTML = rgbToHex(red_val, green_val, blue_val);
     console.log(red_val,green_val,blue_val)
 }
 
 var rgb = document.getElementById("rgb");
+var hsv = document.getElementById("hsv")
 var hex = document.getElementById("hex");
 
 document.getElementById("red").oninput = function() {
