51c1ec4ac274f81722765a8bb8e0ca4c4b194b3d
Manshu Sharma <manshu.sharma@digite.com>
2023-06-08 23:46:55 +0530

    add: paragraph to json feature

--- menu/json-formatter/scripts.js
+++ menu/json-formatter/scripts.js
@@ -1,7 +1,8 @@
 "use strict";
 
 const refs = {
-  button: document.querySelector("#json_beautifier"),
+  json_beautifier: document.querySelector("#json_beautifier"),
+  json_paragraph_convert: document.querySelector("#json_paragraph_convert"),
   textInput: document.querySelector("#text"),
   textOutput: document.querySelector("#result")
 };
@@ -48,4 +49,30 @@ function convertString(e) {
   }
 }
 
-refs.button.addEventListener("click", convertString);
+function paragraphToJson() {
+  const textarea = refs.textInput;
+  const content = textarea.value;
+  
+  // Split the content by newline (\n) character
+  const lines = content.split(/\r?\n/);
+  
+  // Create an empty JavaScript object
+  const json = {};
+  
+  // Iterate over the lines and assign each line as a new key in the JSON object
+  for (let i = 0; i < lines.length; i++) {
+    json[`line${i + 1}`] = lines[i];
+  }
+  
+  // Convert the JavaScript object to JSON string  
+  try {
+    refs.textOutput.value = JSON.stringify(json);
+  }
+  catch (e) {
+    refs.textOutput.value = "Can not parse input string.";
+    console.dir(e);
+  }
+}
+
+refs.json_beautifier.addEventListener("click", convertString);
+refs.json_paragraph_convert.addEventListener("click", paragraphToJson);
